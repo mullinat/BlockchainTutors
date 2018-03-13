0a83fc1b7a471052e0dd5eb331941fceafe7148b
Paul Mullins <paulmullins@protonmail.com>
2018-03-13 15:47:45 -0500

    I now have a worldflow for writing and testing the contracts in the front end

--- Contracts/BlockchainTutorAgency/BlockchainTutorAgency.sol
+++ Contracts/BlockchainTutorAgency/BlockchainTutorAgency.sol
@@ -3,3 +3,11 @@ pragma solidity ^0.4.16;
 contract BlockchainTutorAgency {
 
 }
+pragma solidity ^0.4.16;
+
+contract C {
+    function f() public pure returns (uint8[5]) {
+        string[4] memory adaArr = ["This", "is", "an", "array"];
+        return ([1, 2, 3, 4, 5]);
+    }
+}
--- Contracts/CreateTutor/CreateTutor.sol
+++ Contracts/CreateTutor/CreateTutor.sol
@@ -6,20 +6,52 @@ contract CreateTutor {
     {
         string name;
         address eth_address;
-        string[] capable_of_tutoring;
+        string capable_of_tutoring;
         string website;
         string ipfs_link;
         string email;
         uint balance;
-        int[] eth_recieved_or_sent;
-        int[] amount_recieved_or_sent;
     }
 
+    mapping (uint => Tutor) public tutors;
+    uint32 public next_tutor_index = 1;
+    address public owner;
+    function CreateTutor() public
+    {
+        owner = msg.sender;
+    }
+    function CreateNewTutor(
+        string tmp_name,
+        string tmp_capable_of_tutoring,
+        string tmp_website,
+        string tmp_ipfs_link,
+        string tmp_email
+    ) public payable
+    {
+        var tmp_tutor = tutors[next_tutor_index];
+        tmp_tutor.name = tmp_name;
+        tmp_tutor.eth_address = msg.sender;
+        tmp_tutor.capable_of_tutoring = tmp_capable_of_tutoring;
+        tmp_tutor.website = tmp_website;
+        tmp_tutor.ipfs_link = tmp_ipfs_link;
+        tmp_tutor.email = tmp_email;
+        tmp_tutor.balance = 0;
+        next_tutor_index = next_tutor_index + 1;
+    }
+
+    /*function get_name(uint tmp_index) public returns(string){
+        return tutors[tmp_index].name;
+    }
+    function get_eth_address(uint tmp_index) public returns(address){
+        return tutors[tmp_index].eth_address;
+    }*/
+}
+/*
     function invoice(
         string to_name,
         address to_address,
         address invoice_amount,
-        string[] capable_of_tutoring,
+        string capable_of_tutoring,
         string website,
         string ipfs_link,
         string email
@@ -27,16 +59,4 @@ contract CreateTutor {
     {
         return([0,1,2,3,4]);
     }
-
-    function CreateTutor(
-        string name,
-        address eth_address,
-        string[] capable_of_tutoring,
-        string website,
-        string ipfs_link,
-        string email
-    ) public payable
-    {
-
-    }
-}
+*/
