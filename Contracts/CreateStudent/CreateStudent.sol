00219938a89fef55f076dcdf30ddb542ef20a780
Paul Mullins <noobie375@live.com>
2018-03-13 18:23:14 -0400

    Basic CreateStudent and CreateTutor contracts are complete

--- Contracts/CreateStudent/CreateStudent.sol
+++ Contracts/CreateStudent/CreateStudent.sol
@@ -4,11 +4,39 @@ contract CreateStudent {
     
     struct Student {
         string name;
-        address eth_address;
         uint balance;
-        string[] need_tutor_for ;
+        string need_tutor_for ;
         string email;
-        int[] eth_recieved_or_sent;
-        int[] amount_recieved_or_sent;
+    }
+
+    mapping (address => Student) public students;
+    address public owner;
+    function CreateTutor() public
+    {
+        owner = msg.sender;
+    }
+    function CreateNewTutor(
+        string tmp_name,
+        string need_tutor_for,
+        string tmp_email
+    ) public payable
+    {
+        var tmp_tutor = students[msg.sender];
+        tmp_tutor.name = tmp_name;
+        tmp_tutor.need_tutor_for = need_tutor_for;
+        tmp_tutor.email = tmp_email;
+        tmp_tutor.balance = 0;
+    }
+    function set_name(string tmp_name) public 
+    {
+        students[msg.sender].name = tmp_name;
+    }  
+    function set_capable_of_tutoring(string tmp_need_tutor_for) public 
+    {
+        students[msg.sender].need_tutor_for = tmp_need_tutor_for;
+    } 
+    function set_email(string tmp_email) public 
+    {
+        students[msg.sender].email = tmp_email;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # BlockchainTutors
 Find a Tutor on the Ethereum Blockchain
+
+The goal of this project is to look up and connect with verified tutors that know what you need. The ability to review tutors is also super important.
+
+Features implemented:
+    Data structures for Tutors and Students are created
+    The ability to set and reset data for Tutors and Students
+Features to implement next:
+    Create data structure to track student tutor transactions
+    Add the ability to send an invoice to a student
+    Create a data strucutre for ratings and verified reviews
+Front end features to implement next:
+    Come up wither general UI on a whiteboard or piece of paper
+Back end features to implement next:
+    Technically a back end defeats the purpose of a dapp right?
+Features to implement in the far future:
+    Encrypted messaging between users on the blockchain or maybe another plaform using public keys
