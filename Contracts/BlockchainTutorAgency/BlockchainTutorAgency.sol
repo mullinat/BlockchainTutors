c6a189215a73aebc85574a7e66cbfa54bb3aa1a6
Paul Mullins <noobie375@live.com>
2018-03-13 20:47:19 -0400

    Invoices now have their own contract, still need to figure out what BlockchainTutorAgency.sol will do

--- Contracts/BlockchainTutorAgency/BlockchainTutorAgency.sol
+++ Contracts/BlockchainTutorAgency/BlockchainTutorAgency.sol
@@ -1,25 +1,12 @@
 pragma solidity ^0.4.16;
 import "../CreateStudent/CreateStudent.sol";
 import "../CreateTutor/CreateTutor.sol";
+import "../InvoiceGenerator/InvoiceGenerator.sol";
 
 contract BlockchainTutorAgency {
     CreateStudent student_class;  
     CreateTutor tutor_class;
+    InvoiceGenerator invoice_class;
+    //I still don't know what I am going to do here
 
-    struct Invoice 
-    {
-        address send_money_to;
-        address reciepient;
-        uint32 amount;
-        string comment;
-    }
-    mapping (uint32 => Invoice) invoices;
-    uint32 next_invoice_num = 0;
-    function create_invoice(address tmp_student, uint32 _amount, string _what_for)public
-    {
-        invoices[next_invoice_num].send_money_to = msg.sender;
-        invoices[next_invoice_num].reciepient = tmp_student;
-        invoices[next_invoice_num].amount = _amount;
-        invoices[next_invoice_num].comment = _what_for;
-    }
 }
