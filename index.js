35f9d7fe81da608f6e06b3fe7b358f8d2ea75201
Paul <paul@MSI-Ubuntu-17.10>
2018-03-17 00:04:11 -0400

    The template engine mustache is now available for use The inial API which really isin't an API is set up for tutor, student, and guest

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var fs = require('fs');
+var mustache = require('mustache');//This is currently not used but will be in the future
 
-app.get('/', (req, res) => res.send('Get your blockchain tutor here at BlockahinTutors!'))
-app.use('/src', express.static('src'))
-app.listen(3042, () => console.log('Example app listening on port 3042!'))
+// routes
+app.use('/src', express.static('src'));
+app.get('/', function (req, res) {
+    fs.readFile('./templates/index.mustache', 'utf8', (err, data) => {
+        if (err) throw err;
+        //res.send(mustache.to_html(data, {name: "Joe"}));
+        res.send(data);
+    });
+});
+app.get('/student', function (req, res) {
+    fs.readFile('./templates/student.mustache', 'utf8', (err, data) => {
+        if (err) throw err;
+        //res.send(mustache.to_html(data, {name: "Joe"}));
+        res.send(data);
+    });
+});
+app.get('/tutor', function (req, res) {
+    fs.readFile('./templates/tutor.mustache', 'utf8', (err, data) => {
+        if (err) throw err;
+        //res.send(mustache.to_html(data, {name: "Joe"}));
+        res.send(data);
+    });
+});
+app.get('/guest', function (req, res) {
+    fs.readFile('./templates/guest.mustache', 'utf8', (err, data) => {
+        if (err) throw err;
+        //res.send(mustache.to_html(data, {name: "Joe"}));
+        res.send(data);
+    });
+});
+
+
+//Host the app
+app.listen(3042, () => console.log('Example app listening on port 3042!'));
