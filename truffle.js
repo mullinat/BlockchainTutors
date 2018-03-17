4d7a25b3fe5db9bf8e498d1ec8073b5d44d2c8c3
Paul <paul@MSI-Ubuntu-17.10>
2018-03-17 12:01:48 -0400

    Seems like the previous problem with truffle was not resolved, thinking maybe the git commit itself had something to do with it

---
+++ truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+    networks: {
+      development: {
+        host: "127.0.0.1",
+        port: 5555,
+        network_id: "*" // Match any network id
+      }
+    }
+  };
+  
