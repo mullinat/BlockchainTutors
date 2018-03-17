9bce4986569bb0c80bd10d0a7e851240f6f883f9
Paul <paul@MSI-Ubuntu-17.10>
2018-03-17 11:49:22 -0400

    Working on truffle bug where it will not deploy

---
+++ TruffleWorkspace/migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+var CreateTutor = artifacts.require("./CreateTutor.sol");
+var CreateStudent = artifacts.require("./CreateStudent.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(CreateTutor);
+  deployer.deploy(CreateStudent);
+};
