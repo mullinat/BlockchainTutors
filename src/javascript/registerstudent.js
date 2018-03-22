b927ff30d01e3f3b06f2b3315b64e3dd3f6b0c0d
Paul <paul@MSI-Ubuntu-17.10>
2018-03-22 11:52:30 -0400

    Basic search for tutor UI now functional

--- src/javascript/registerstudent.js
+++ src/javascript/registerstudent.js
@@ -6,12 +6,12 @@ $.getJSON("./abi/CreateStudent.json", function (result) {
     //console.log(field);
     MyContractABI = result;
     var tmp_networks = [];
-    for (var i in MyContractABI.networks){
+    for (var i in MyContractABI.networks) {
         tmp_networks.push(i);
     }
-    console.log("tmp_networks = " + tmp_networks);
-    console.log(tmp_networks[tmp_networks.length-1]);
-    var tmp_the_correct_network = tmp_networks[tmp_networks.length-1];
+    //console.log("tmp_networks = " + tmp_networks);
+    //console.log(tmp_networks[tmp_networks.length - 1]);
+    var tmp_the_correct_network = tmp_networks[tmp_networks.length - 1];
     var address = MyContractABI.networks[tmp_the_correct_network].address;
     MyContract = web3.eth.contract(MyContractABI.abi).at(address);
     MyContract.students(web3.eth.coinbase, function (err, result) {
@@ -20,19 +20,20 @@ $.getJSON("./abi/CreateStudent.json", function (result) {
         StudentInformation = result;
         if (result[0] == "") {
             registerstudent();
-        }
-        else{
+        } else {
             editstudent();
         }
     });
 });
+
 function editstudent() {
     $.get("./src/html/editstudent.html", function (result) {
         $("body").append(result);
         showStudentInformation();
     })
 }
-function showStudentInformation(){
+
+function showStudentInformation() {
     $("#existingStudentInformation").append("<p>Name : " + StudentInformation[0] + "</p>");
     $("#existingStudentInformation").append("<p>Topics of Interest : " + StudentInformation[2] + "</p>");
     $("#existingStudentInformation").append("<p>email : " + StudentInformation[3] + "</p>");
@@ -59,18 +60,36 @@ function CreateStudent() {
     });
 }
 
-function UpdateStudent(tmpID){
-    switch(tmpID){
+function UpdateStudent(tmpID) {
+    switch (tmpID) {
         case "student_name":
-            MyContract.setName(document.getElementById(tmpID).value, function(){console.log("Ummm what now?")});
+            MyContract.setName(document.getElementById(tmpID).value, function () {
+                console.log("Ummm what now?")
+            });
             break;
         case "student_subjects":
-            MyContract.setCapableOfTutoring(document.getElementById(tmpID).value, function(){console.log("Ummm what now?")});
+            MyContract.setCapableOfTutoring(document.getElementById(tmpID).value, function () {
+                console.log("Ummm what now?")
+            });
             break;
         case "student_email":
-            MyContract.setEmail(document.getElementById(tmpID).value, function(){console.log("Ummm what now?")});
+            MyContract.setEmail(document.getElementById(tmpID).value, function () {
+                console.log("Ummm what now?")
+            });
             break;
         default:
             console.log("Something went wrong");
     }
+}
+var find_tutor_displayed = false;
+
+function find_a_tutor() {
+    //console.log("find_a_tutor function");
+    $.get("/src/html/findatutor.html", function (result) {
+        //console.log(result);
+        if (find_tutor_displayed == false) {
+            $("#tutor_search").append(result);
+            find_tutor_displayed = true;
+        }
+    })
 }
