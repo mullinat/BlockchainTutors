c251ac87601a37d6f097c361ebc5a668bac51183
Paul <paul@MSI-Ubuntu-17.10>
2018-03-24 15:08:56 -0400

    basic routes for new features implemented

--- server.js
+++ server.js
@@ -33,6 +33,27 @@ app.get('/guest', function (req, res) {
         res.send(data);
     });
 });
+app.get('/createinvoice', function (req, res) {
+    fs.readFile('./templates/createinvoice.mustache', 'utf8', (err, data) => {
+        if (err) throw err;
+        //res.send(mustache.to_html(data, {name: "Joe"}));
+        res.send(data);
+    });
+});
+app.get('/manageinvoices', function (req, res) {
+    fs.readFile('./templates/manageinvoices.mustache', 'utf8', (err, data) => {
+        if (err) throw err;
+        //res.send(mustache.to_html(data, {name: "Joe"}));
+        res.send(data);
+    });
+});
+app.get('/yourbalance', function (req, res) {
+    fs.readFile('./templates/yourbalance.mustache', 'utf8', (err, data) => {
+        if (err) throw err;
+        //res.send(mustache.to_html(data, {name: "Joe"}));
+        res.send(data);
+    });
+});
 
 
 //Host the app
