5b9315c4862f20653b960c2977f973deac27bf14
Paul Mullins <paulmullins@protonmail.com>
2018-03-30 11:05:48 -0400

    Setting up truffle to work with infura

--- TruffleWorkspace/truffle.js
+++ TruffleWorkspace/truffle.js
@@ -1,9 +1,18 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var privateKey = "I PUT MY PRIVATE KEY IN A PUBLIC REPO LMAFO";
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 5555,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/<INFURA_Access_Token>")
+      },
+      network_id: 3
+    } 
   }
 };
