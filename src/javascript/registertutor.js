e30a7204b88c39e0db9cc6421bd88a84addde1af
Paul Mullins <paulmullins@protonmail.com>
2018-03-31 13:21:36 -0400

    Continoued tring to debug CreateTutor2 Contract

--- src/javascript/registertutor.js
+++ src/javascript/registertutor.js
@@ -1,3 +1,4 @@
+//
 var MyContractABI;
 var MyContract;
 var TutorInformation;
@@ -6,48 +7,51 @@ $.getJSON("./abi/CreateTutor.json", function (result) {
     //console.log(field);
     MyContractABI = result;
     var tmp_networks = [];
-    for (var i in MyContractABI.networks){
+    for (var i in MyContractABI.networks) {
         tmp_networks.push(i);
     }
     console.log("tmp_networks = " + tmp_networks);
-    console.log(tmp_networks[tmp_networks.length-1]);
-    var tmp_the_correct_network = tmp_networks[tmp_networks.length-1];
+    console.log(tmp_networks[tmp_networks.length - 1]);
+    var tmp_the_correct_network = tmp_networks[tmp_networks.length - 1];
     var address = MyContractABI.networks[tmp_the_correct_network].address;
     MyContract = web3.eth.contract(MyContractABI.abi).at(address);
     MyContract.tutors(web3.eth.coinbase, function (err, result) {
         //console.log(result);
         //console.log(result[0]);
         TutorInformation = result;
-        if (result[0] == "" ) {
+        if (result[0] == "") {
             registertutor();
         }
-        else{
+        else {
             edittutor();
         }
     });
 });
-function edittutor() {
-    $.get("./src/html/edittutor.html", function (result) {
-        $("body").append(result);
-        showTutorInformation();
-    })
-}
-function showTutorInformation(){
-    $("#showTutorInformation").append("<p>Name : " + TutorInformation[0] + "</p>");
-    $("#showTutorInformation").append("<p>Topics of Interest : " + TutorInformation[1] + "</p>");
-    $("#showTutorInformation").append("<p>Website : " + TutorInformation[2] + "</p>");
-    $("#showTutorInformation").append("<p>IPFS : " + TutorInformation[3] + "</p>");
-    $("#showTutorInformation").append("<p>email : " + TutorInformation[4] + "</p>");
-}
 
 function registertutor() {
     $.get("./src/html/registertutor.html", function (result) {
         $("body").append(result);
     })
 }
-
 var tutor_info_ids = ['tutor_name', 'tutor_subjects', 'tutor_website', 'tutor_IPFS', 'tutor_email']
 var tutor_info = [];
+
+//
+GetSmartContract("/abi/CreateTutor2.json", "CreateTutor2");
+function edittutor() {
+    $.get("./src/html/edittutor.html", function (result) {
+        $("body").append(result);
+        showTutorInformation();
+    })
+}
+
+function showTutorInformation() {
+    $("#showTutorInformation").append("<p>Name : " + TutorInformation[0] + "</p>");
+    $("#showTutorInformation").append("<p>Topics of Interest : " + TutorInformation[1] + "</p>");
+    $("#showTutorInformation").append("<p>Website : " + TutorInformation[2] + "</p>");
+    $("#showTutorInformation").append("<p>IPFS : " + TutorInformation[3] + "</p>");
+    $("#showTutorInformation").append("<p>email : " + TutorInformation[4] + "</p>");
+}
 
 function CreateTutor() {
     for (var tmp_item in tutor_info_ids) {
@@ -56,29 +60,34 @@ function CreateTutor() {
         tutor_info.push(document.getElementById(tutor_info_ids[tmp_item]).value);
         document.getElementById(tutor_info_ids[tmp_item]).value = "";
     }
-    MyContract.createNewTutor(tutor_info[0], tutor_info[1], tutor_info[2], tutor_info[3], tutor_info[4], function () {
+    SmartContracts["CreateTutor2"].call.createNewTutor(tutor_info[0], tutor_info[1], tutor_info[2], tutor_info[3], tutor_info[4], function (err, result) {
+        console.log("error is" + err);
+        console.log("result is " + result);
         console.log("Information deployed successfully")
     });
 }
 
-function UpdateTutor(tmpID){
-    switch(tmpID){
+function UpdateTutor(tmpID) {
+    switch (tmpID) {
         case "tutor_name":
-            MyContract.setName(document.getElementById(tmpID).value, function(){console.log("Ummm what now?")});
+            SmartContracts["CreateTutor2"].call.setName(document.getElementById(tmpID).value, function () { console.log("Ummm what now?") });
             break;
         case "tutor_subjects":
-            MyContract.setCapableOfTutoring(document.getElementById(tmpID).value, function(){console.log("Ummm what now?")});
+            SmartContracts["CreateTutor2"].call.setCapableOfTutoring(document.getElementById(tmpID).value, function () { console.log("Ummm what now?") });
             break;
         case "tutor_website":
-            MyContract.setWebsite(document.getElementById(tmpID).value, function(){console.log("Ummm what now?")});
+            SmartContracts["CreateTutor2"].call.setWebsite(document.getElementById(tmpID).value, function () { console.log("Ummm what now?") });
             break;
         case "tutor_IPFS":
-            MyContract.setIpfsLink(document.getElementById(tmpID).value, function(){console.log("Ummm what now?")});
+            SmartContracts["CreateTutor2"].call.setIpfsLink(document.getElementById(tmpID).value, function () { console.log("Ummm what now?") });
             break;
         case "tutor_email":
-            MyContract.setEmail(document.getElementById(tmpID).value, function(){console.log("Ummm what now?")});
+            SmartContracts["CreateTutor2"].call.setEmail(document.getElementById(tmpID).value, function () { console.log("Ummm what now?") });
             break;
         default:
             console.log("Something went wrong");
     }
+}
+function SmartContractLoaded() {
+    SmartContracts["CreateTutor2"].call.tutors(0, function (err, result) { TutorInformation = result });
 }
