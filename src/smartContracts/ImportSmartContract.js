3308517642efc5a8824418f5409819b5cef3912a
Paul Mullins <paulmullins@protonmail.com>
2018-03-31 11:34:20 -0400

    ImportSmartContract.js now stores all the input contracts as objects instead of in a list

--- src/smartContracts/ImportSmartContract.js
+++ src/smartContracts/ImportSmartContract.js
@@ -1,23 +1,23 @@
 var SmartContract = { name: "", abi: "", address: "", call: "" }
-var SmartContracts = [];
+var SmartContracts = {};
 //var GenerateInvoiceABI;
 //var GenerateInvoiceAddress;
 //var GenerateInvoice;
 function GetSmartContract(_location, _name) {
     $.getJSON(_location, function (result) {
         //GenerateInvoiceABI = result;
-        SmartContracts.push(SmartContract);
-        SmartContracts[SmartContracts.length - 1].abi = result;
+        SmartContracts[_name] = SmartContract;
+        SmartContracts[_name].abi = result;
         var tmp_networks = [];
-        for (var i in SmartContracts[SmartContracts.length - 1].abi.networks) {
+        for (var i in SmartContracts[_name].abi.networks) {
             tmp_networks.push(i);
         }
-        SmartContracts[SmartContracts.length - 1].name = _name;
+        SmartContracts[_name].name = _name;
         //console.log("tmp_networks = " + tmp_networks);
         //console.log(tmp_networks[tmp_networks.length - 1]);
         var tmp_the_correct_network = tmp_networks[tmp_networks.length - 1];
-        console.log(SmartContracts[SmartContracts.length - 1].abi)
-        SmartContracts[SmartContracts.length - 1].address = SmartContracts[SmartContracts.length - 1].abi.networks[tmp_the_correct_network].address;
-        SmartContracts[SmartContracts.length - 1].call = web3.eth.contract(SmartContracts[SmartContracts.length - 1].abi.abi).at(SmartContracts[SmartContracts.length - 1].address);
+        console.log(SmartContracts[_name].abi)
+        SmartContracts[_name].address = SmartContracts[_name].abi.networks[tmp_the_correct_network].address;
+        SmartContracts[_name].call = web3.eth.contract(SmartContracts[_name].abi.abi).at(SmartContracts[_name].address);
     });
 }
