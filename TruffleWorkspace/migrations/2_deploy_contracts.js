eaeb28c9700221b1b46efd104c7e3c40f2f12e17
Paul Mullins <paulmullins@protonmail.com>
2018-03-31 17:21:25 -0400

    Contracts calling contracts now works

--- TruffleWorkspace/migrations/2_deploy_contracts.js
+++ TruffleWorkspace/migrations/2_deploy_contracts.js
@@ -12,6 +12,6 @@ module.exports = function (deployer) {
   deployer.deploy(InvoiceGenerator);
   
   //You may want to change these to and address you control 
-  deployer.deploy(BlockAppsData, "0x2e874B4997D5074c6E39729cD27d20166CEB1bf7");
-  deployer.deploy(CreateTutor2, "0x2e874B4997D5074c6E39729cD27d20166CEB1bf7");
+  deployer.deploy(BlockAppsData, "0x00865f76187215724EE066bab71F74d6406A0ebE");
+  deployer.deploy(CreateTutor2, "0x00865f76187215724EE066bab71F74d6406A0ebE");
 };
